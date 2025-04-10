9e9db0ac8b3a1b8bbe26de8ed4429ff6ab5c28a8
Sebastian Ruiz <sebastianruizpulache@gmail.com>
2025-04-10 15:24:01 -0500

    Entregable final

--- src/breakingbad/breakingbad-app.js
+++ src/breakingbad/breakingbad-app.js
@@ -19,8 +19,39 @@ export const BreakingBadApp = async (el) => {
     <h3>Loading ...</h3>
     `;
 
-  const quote = await fetchQuote()
-  el.innerHTML = `
-  <h3>¡Tenemos data!.</h3>
-  `;
+  // const quote = await fetchQuote()
+  // el.innerHTML = `
+  // <h3>¡Tenemos data!.</h3>
+  // `;
+
+  const quoteLabel = document.createElement('blockquote');
+  const authorLabel = document.createElement('h3');
+  const nextQuoteButton = document.createElement('button');
+  nextQuoteButton.innerText = 'Next Quote';
+
+  const renderQuote = (data) => {
+    quoteLabel.innerHTML = data.quote;
+    authorLabel.innerHTML = data.author;
+    el.replaceChildren(quoteLabel, authorLabel, nextQuoteButton);
+  }
+
+  nextQuoteButton.addEventListener('click', async () => {
+    el.innerHTML = 'Loading...';
+    const quote = await fetchQuote();
+    renderQuote(quote)
+  })
+  // una proxima mejora seria q la logica  de invocar a la peticion este en una funcion y nosostros pongamos aqui nomas la llamada
+  // resumen q se llame x default y x click al boton
+
+  fetchQuote()
+    .then(renderQuote);
+  // .then(data => renderQuote(data));
+
 }
+// esta en mayus xk quiero indicar q es un componente
+// Es una pieza reutilizable de mi aplicación, que si ustedes la mandan a llamar va a realizar cierto trabajo.
+
+// https://dog.ceo/api/breeds/image/random
+
+//  curl -s "https://api.breakingbadquotes.xyz/v1/quotes" | python -m json.tool
+// curl -s "https://api.breakingbadquotes.xyz/v1/quotes/5" | python -m json.tool
