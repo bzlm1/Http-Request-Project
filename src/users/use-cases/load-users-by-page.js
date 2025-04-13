de3238ab76e2daf7cdef93da6ac5bdc4946eccc4
Sebastian Ruiz <sebastianruizpulache@gmail.com>
2025-04-13 10:05:25 -0500

    La funcionalidad de botones funcionan, sin embargo el next es infinito

--- src/users/use-cases/load-users-by-page.js
+++ src/users/use-cases/load-users-by-page.js
@@ -6,11 +6,9 @@ import { User } from '../models/user.js';
  * @returns {Promise<User[]>} user information
  */
 export const loadUsersByPage = async (page = 1) => {
-  // const url = `${import.meta.env.VITE_BASE_URL}/users?page=${page}`;
-  const url = `${import.meta.env.VITE_BASE_URL}/users?_page=${page}&_limit=10`;
-
+  const url = `${import.meta.env.VITE_BASE_URL}/users?_page=${page}`;
   const res = await fetch(url);
-  const data = await res.json();
+  const { first, last, data } = await res.json();
 
   const users = data.map(localhostUserToModel);
   return users;
